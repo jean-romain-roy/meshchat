c30c00095290d76bc6c1b7c46a573b6ea840a5de
jean-romain <jean-romain@outlook.com>
2019-01-19 12:50:53 -0500

    UI setup

---
+++ app/src/main/java/com/example/meshchat/Fragment_Forum.java
@@ -0,0 +1,21 @@
+package com.example.meshchat;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class Fragment_Forum extends Fragment {
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_forum, container, false);
+
+
+
+        return rootView;
+    }
+
+}
